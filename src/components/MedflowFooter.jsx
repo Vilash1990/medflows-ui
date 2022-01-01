352ec3c75a569e7da1992934445d8b5c90a02ca0
Vilash1990 <ashokkumarmaricharla@gmail.com>
2022-01-01 16:27:23 -0600

    Updated Footer

--- src/components/MedflowFooter.jsx
+++ src/components/MedflowFooter.jsx
@@ -1,16 +1,16 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Navbar, NavbarBrand } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCopyright } from "@fortawesome/free-solid-svg-icons";
 const MedflowFooter = () => {
   return (
-    <Container fluid className="shadow rounded medflowFooter text-center bg-light m">
-      <Row>
-        <Col sm={"12"}>
-          <FontAwesomeIcon icon={faCopyright} size="sm" />
-          2018-2022 Medflow Solutions Inc., All rights reserved
-        </Col>
-      </Row>
-    </Container>
+     <div className="fixed-bottom text-center">  
+     <Navbar color="dark" dark>
+         <Container>
+             <small className="mx-auto" ><FontAwesomeIcon icon={faCopyright} size="sm" />
+          2018-2022 Medflow Solutions Inc., All rights reserved</small>
+         </Container>
+     </Navbar>
+ </div>
   );
 };
 
